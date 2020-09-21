94ab9192b8827d2c02d2413feab2e1cbafa668a3
NikolayTolkovanov <71602188+NikolayTolkovanov@users.noreply.github.com>
2020-09-21 21:18:23 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,17 @@
+GAME_NAME = 'Breakout Lite'
+
+WIDTH = 600
+HEIGHT = 600
+
+BALL_SIZE = '30'
+BALL_COLOR = 'blueviolet'
+
+PUDDLE_WIDTH = 20
+PUDDLE_HEIGHT = 100
+PUDDLE_COLOR = 'blue'
+
+SCORE_SIZE = 30
+SCORE_COLOR = 'green'
+
+END_TEXT_SIZE = 50
+END_TEXT_COLOR = 'red'
